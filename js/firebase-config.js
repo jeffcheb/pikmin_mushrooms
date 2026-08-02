410d6f38c5608ce2a5a8d301bc5fb7ba0708de37
jeffcheb <jeff110chen@gmail.com>
2026-08-02 20:58:41 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -33,5 +33,16 @@ window.fbRemove = remove;
 window.fbOnValue = onValue;
 window.fbQuery = query;
 window.fbLimitToLast = limitToLast;
+// js/firebase-config.js
 
+const firebaseConfig = {
+    apiKey: "eMV7OAD1nRr0X4ZRx2AGyghYogK7DlFZEL9QiZTI",
+    authDomain: "https://console.firebase.google.com/project/pikmin-mushroom-hub/database/pikmin-mushroom-hub-default-rtdb/data/~2F?hl=zh-cn",
+    // 🌟 關鍵！一定要有這一行 Realtime Database 的完整網址：
+    databaseURL: "https://pikmin-mushroom-hub-default-rtdb.firebaseio.com",
+    projectId: "pikmin-mushroom-hub",
+    storageBucket: "pikmin-mushroom-hub.appspot.com",
+    messagingSenderId: "1234567890",
+    appId: "1:1234567890:web:abcdef123456"
+};
 console.log("🔥 皮克敏工具站：Firebase 雲端資料庫連線成功！");
